943285cc7a6d8231cff96d977c8344f595723765
shujah <rehmanshujah57@gmail.com>
2025-11-26 17:42:31 +0500

    day 5 ci with coverage

---
+++ tests/test_app.py
@@ -0,0 +1,10 @@
+from app import add, subtract, multiply
+
+def test_add():
+    assert add(2, 3) == 5
+
+def test_subtract():
+    assert subtract(10, 4) == 6
+
+def test_multiply():
+    assert multiply(3, 4) == 12
